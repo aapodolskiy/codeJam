fb7ba29a915ea33d6f868753d9b76bab65e7da80
Alexander <a.a.podolskiy@gmail.com>
2019-11-05 18:14:42 +0300

    all 3 tests passed)

--- Foregone_Solution.cpp
+++ Foregone_Solution.cpp
@@ -6,30 +6,36 @@ using namespace std;
 int main() {
   int T = 0;
   cin >> T;
-    
-  string line;
-  getline(cin, line);
+
+  string lines[100];
+
+  getline(cin, lines[0]);
   
+  for (int i = 0; i < T; ++i) {
+    getline(cin, lines[i]);
+  }
+
   string a[100];
   string b[100];
 
-  for (int i = 0; i < T; ++i) {
-    getline(cin, line);
-    
-    for (int j = 0; j < line.length(); ++j) {
-      if (line[j] == '4') {
-        a[i].append("2");
-        b[i].append("2");
+  for (int i = 0; i< T; ++i) {
+    a[i] = b[i] = "";
+
+    for (int j = 0; j < lines[i].length(); ++j) {
+      if (lines[i][j] == '4') {
+        a[i] += "2";
+        b[i] += "2";
       } else {
-        a[i].append(line[j]);
-        b[i].append("0");
+        a[i] += lines[i][j];
+        if (b[i].length() > 0) {
+          b[i] += "0";
+        }
       }
     }
-    
   }
 
   for (int i = 0; i < T; ++i) {
-    cout << a[i] << " " << b[i] << endl;
+    cout << "Case #"<< i+1 << ": " << a[i] << " " << b[i] << endl;
   }
   
   return 0;
